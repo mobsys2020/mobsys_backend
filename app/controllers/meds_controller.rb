d70d6a015f1725b8009bb5c746c145b6b6b65feb
Ole H <cerberus885@gmail.com>
2020-06-17 17:16:38 +0200

    now adds quantity modifier on its own

--- app/controllers/meds_controller.rb
+++ app/controllers/meds_controller.rb
@@ -4,6 +4,7 @@ class MedsController < ApplicationController
         @med = Med.new
         @med.name = params[:name]
         @med.quantity = params[:quantity].to_s
+        @med.quantity += params[:_quantity]
         @med.time = params[:time].to_s
         @med.days = params[:days].to_s
         @med.medplan_id = params[:medplan_id]
